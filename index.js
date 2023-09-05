f283bc25da9822c0f00572323a006366942a5a4b
Perera Dev <nashi.codes@gmail.com>
2023-09-05 18:59:02 -0300

    update logic

--- index.js
+++ index.js
@@ -1,35 +1,50 @@
 const puppeteer = require("puppeteer");
-const {AxePuppeteer} = require("@axe-core/puppeteer");
+const { AxePuppeteer } = require("@axe-core/puppeteer");
 const fs = require("fs");
 
 (async () => {
-    const raw_translate = fs.readFileSync(
-        "./node_modules/axe-core/locales/pt_BR.json"
-    );
-    const translate = JSON.parse(raw_translate);
+  const raw_translate = fs.readFileSync(
+    "./node_modules/axe-core/locales/pt_BR.json"
+  );
+  const translate = JSON.parse(raw_translate);
 
-    //The URL of the page to be evaluated
-    const url = "";
+  //The URL of the page to be evaluated
+  let url = "";
 
-    const browser = await puppeteer.launch({headless: true});
-    const page = await browser.newPage();
-    await page.setBypassCSP(true);
+  let browser;
+  let page;
+  try {
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
     await page.goto(url);
 
     const config = {
-        locale: translate,
+      locale: translate,
     };
-    try {
-        const results = await new AxePuppeteer(page)
-            .configure(config)
-            .include(".wp_innerpage_header").analyze();
-        if (fs.existsSync("./results") === false)
-            fs.mkdirSync("./results");
-        console.log(results);
-        fs.writeFileSync("./results/".concat("result.json"), JSON.stringify(results));
-    } catch (err) {
-        console.log(err);
+
+    const results = await new AxePuppeteer(page).configure(config).analyze();
+
+    const dir = "/results/".concat(urlToFoldes(results.url));
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
     }
 
-    await browser.close();
+    console.log(results);
+
+    fs.writeFileSync(url.concat("result.json"), JSON.stringify(results));
+  } catch (err) {
+    console.log(err);
+  }
+  await page.close();
+  await browser.close();
 })();
+
+function urlToFoldes(url) {
+  url = url.replace(/(^\w+:|^)\/\//, "");
+  url = url.replace(/\./g, "_");
+  url = url.split("/");
+  if (url[url.length - 1] === "") url.pop();
+  url = url.join("/");
+  return url;
+}
