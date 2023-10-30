9a497beb719be0ac1e5b896dc9b25d57b92bef97
Perera Dev <nashi.codes@gmail.com>
2023-10-30 15:31:03 -0300

    refactoring directories

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const { PRIVATE_KEY, user, tokenValited } = require("./auth");
+const {PRIVATE_KEY, user, tokenValited} = require("./auth");
 const fs = require("fs");
 
 const app = express();
@@ -8,46 +8,52 @@ const app = express();
 app.use(express.json());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+    console.log("Server is running on port 3000.");
 });
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Hello World!");
+    return res.status(200).send("Hello World!");
 });
 
 app.get("/login", (req, res) => {
-  const [, hash] = req.headers.authorization?.split(" ") || [" ", " "];
-  const [email, password] = Buffer.from(hash, "base64").toString().split(":");
+    const [, hash] = req.headers.authorization?.split(" ") || [" ", " "];
+    const [email, password] = Buffer.from(hash, "base64").toString().split(":");
 
-  try {
-    const correctPassword = email === user.email && password === "123456";
+    try {
+        const correctPassword = email === user.email && password === "123456";
 
-    if (!correctPassword)
-      return res.status(401).send("Usuário ou senha inválidos");
+        if (!correctPassword) return res.status(401).send("Usuário ou senha inválidos");
 
-    const token = jwt.sign({ user: JSON.stringify(user) }, PRIVATE_KEY, {
-      expiresIn: "1h",
-    });
+        const token = jwt.sign({user: JSON.stringify(user)}, PRIVATE_KEY, {
+            expiresIn: "1h",
+        });
 
-    return res.status(200).json({ data: { user, token } });
-  } catch (error) {
-    console.log(error);
-    return res.send(error);
-  }
+        return res.status(200).json({data: {user, token}});
+    } catch (error) {
+        console.log(error);
+        return res.send(error);
+    }
 });
 
 app.use("*", tokenValited);
 
 app.get("/getOne/:id", (req, res) => {
-  try {
-    const id = req.params.id;
-    const rawdata = fs.readFileSync(
-      "./reports/".concat(id.concat("-emag.json"))
-    );
-    const report = JSON.parse(rawdata);
-    res.statusCode = 200;
-    res.json(report);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
+    try {
+        const id = req.params.id;
+        const rawdata = fs.readFileSync("./reports/".concat(id.concat("-emag.json")));
+        const report = JSON.parse(rawdata);
+        res.statusCode = 200;
+        res.json(report);
+    } catch (err) {
+        switch (err.code) {
+            case "ENOENT":
+                res.statusCode = 404;
+                res.json({error: "Report not found"});
+                break;
+            default:
+                res.statusCode = 500;
+                res.json({error: "Internal server error"});
+                break;
+        }
+    }
 });
